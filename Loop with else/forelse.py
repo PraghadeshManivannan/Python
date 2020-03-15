211a4c97d32827d89d362c3b87c344458866db4d
Praghadesh <61776225+PraghadeshManivannan@users.noreply.github.com>
2020-03-15 22:01:47 +0530

    Add files via upload

---
+++ Loop with else/forelse.py
@@ -0,0 +1,8 @@
+a = [1,2,3,4,5,6]
+c = 'hi'
+for i in a:
+    if c == i:
+        print(c,"Found")
+else:
+    print (c,"Not found")
+    print (' the For/Else Statement! is called')
---
+++ Loop with else/whileelse.py
@@ -0,0 +1,10 @@
+i = 1
+while i<2:
+    print('Inside 1st while scope')
+    i += 1
+
+while i<2:
+    print('Inside 2nd while scope')
+    i += 1
+else:
+    print (' the While/Else Statement! is called')
